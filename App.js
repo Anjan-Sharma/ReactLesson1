6e73205e77c98666ddfcf76456baab19d5e70da3
Anjan Sharma <anjansharma232@gmail.com>
2023-05-27 15:25:40 +0545

    Create html elements using react

--- App.js
+++ App.js
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 console.log("Document Connected");
+// console.log(React);
+// console.log(ReactDOM);
+
+const heading = React.createElement("h1", { id: "title" }, "Heading1");
+const div = React.createElement("div", { id: "container" }, [heading]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(div);
